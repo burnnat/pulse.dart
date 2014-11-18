15c9563a59c01a760f075875ff88e092be8dc6c2
Nat Burns <nbaccount@burnskids.com>
2014-11-17 22:16:43 -0500

    Dynamically create package symlinks for command-line tests.
    This is necessary for compatibility with Chrome Dev Editor, since
    the editor will not compile against linked directories. In this
    fashion, we remove the symlinks immediately after the tests complete.

--- test/test.dart
+++ test/test.dart
@@ -1,3 +1,25 @@
-import '../web/test/test.dart';
+import 'dart:io';
+import 'dart:isolate';
 
-void main() => runTests();
+void main() {
+  Directory dir = new File.fromUri(Platform.script).parent.parent;
+
+  Link link = new Link('${dir.path}/web/test/packages');
+  link.createSync('${dir.path}/packages');
+
+  ReceivePort port = new ReceivePort();
+
+  Isolate
+    .spawnUri(
+      Uri.parse('${dir.path}/web/test/test.dart'),
+      ['testing'],
+      port.sendPort
+      // Rather than manually creating symlinks, the idiomatic
+      // approach would be to use the 'packageRoot' argument:
+      // packageRoot: Uri.parse('${dir.path}/packages')
+      // Unfortunately, not all platforms support this.
+    )
+    .then((_) => port.first);
+
+  link.deleteSync();
+}
--- web/test/test.dart
+++ web/test/test.dart
@@ -1,6 +1,24 @@
-import 'package:unittest/unittest.dart';
+import 'dart:isolate';
+
+import 'package:unittest/unittest.dart' as unittest;
 import 'message_test.dart' as message;
 
 void runTests() {
   message.runTests();
+}
+
+class WaitConfiguration extends unittest.SimpleConfiguration {
+  final SendPort port;
+
+  WaitConfiguration(this.port);
+
+  void onDone(bool passed) {
+    super.onDone(passed);
+    port.send(passed);
+  }
+}
+
+void main(List<String> args, SendPort port) {
+  unittest.unittestConfiguration = new WaitConfiguration(port);
+  runTests();
 }
